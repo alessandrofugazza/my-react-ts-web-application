f2b4ea147217fc39c8f04569dcb6cef54c355232
Alessandro Fugazza <137590109+alessandrofugazza@users.noreply.github.com>
2026-07-11 15:17:03 +0200

    something again

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,3 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://immdxaerayxmdydoxcjq.supabase.co'
-const supabaseKey = 'sb_publishable_NIJJEX9j5UCsH1omiARrFw_f5NZ3zQ5'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY)
